17bb3b74776a8e6ccb3585b082d83e2c0ed64108
Tsimmuaj <tyang4204@gmail.com>
2024-09-26 13:00:47 -0500

    to dict test

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -17,5 +17,10 @@ class test(unittest.TestCase):
 		save2 = a.updated_at
 		self.assertNotEqual(save1, save2)
 
+	def test_to_dict(self):
+		a = BaseModel()
+		my_dict = a.to_dict()
+		self.assertEqual(type(my_dict), dict)
+
 if __name__ == "__main__":
 	unittest.main()
