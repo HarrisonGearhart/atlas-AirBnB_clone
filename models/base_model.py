454f7071813f0af480a9ce16b6da32fb4810f907
Tsimmuaj <tyang4204@gmail.com>
2024-09-26 11:29:39 -0500

    def innit

--- models/base_model.py
+++ models/base_model.py
@@ -4,8 +4,11 @@ This is a file holding a base class that will be inherited by
 many other classes
 """
 import uuid
+from datetime import datetime
 
 
 class BaseModel:
-	def __init__(self, id, created_at, updated_at):
+	def __init__(self):
 		self.id = str(uuid.uuid4())
+		self.created_at = datetime.now()
+		self.updated_at = datetime.now()
