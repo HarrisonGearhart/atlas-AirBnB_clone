f0b8f7b64ec830afd36e203d186921bd312af44d
Harrison  Gearhart <harrisongearhart@Harrisons-MacBook-Air.local>
2024-09-26 12:31:30 -0500

    updated BaseModel Class with args and kwargs

--- models/base_model.py
+++ models/base_model.py
@@ -22,9 +22,9 @@ class BaseModel:
 				else:
 					self.__dict__[key] = kwargs[key]
 		else:
-		self.id = str(uuid.uuid4())
-		self.created_at = datetime.now()
-		self.updated_at = datetime.now()
+			self.id = str(uuid.uuid4())
+			self.created_at = datetime.now()
+			self.updated_at = datetime.now()
 
 	def __str__(self):
 		return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
