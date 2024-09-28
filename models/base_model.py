36d12c384c666190cd984460199f6ab6c1e4ea7e
Tsimmuaj <tyang4204@gmail.com>
2024-09-28 15:17:49 -0500

    worked through file_storage

--- models/base_model.py
+++ models/base_model.py
@@ -5,6 +5,7 @@ many other classes
 """
 import uuid
 from datetime import datetime
+from models.__init__ import storage
 
 
 class BaseModel:
@@ -19,12 +20,15 @@ class BaseModel:
                     self.__dict__["updated_at"] = datetime.strptime(
                         kwargs["created_at"], "%Y-%m-%dT%H:%M:%S.%f"
                     )
+                elif key == "id":
+                    self.__dict__["id"] = str(uuid.uuid4())
                 else:
                     self.__dict__[key] = kwargs[key]
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+            storage.new(self.to_dict())
 
     def __str__(self):
         return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
@@ -34,6 +38,7 @@ class BaseModel:
         updates the updated_at attribute
         """
         self.updated_at = datetime.now()
+        storage.save()
 
     def to_dict(self):
         """
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,34 +1,43 @@
 #!/usr/bin/python3
 """Module for FileStorage class
 serializes instances to a JSON file and deserializes JSON file to instances"""
+import json
 
 
 class FileStorage:
-	"""
-	Class for storing and retrieving data
-	Private class attributes:
-		__file_path: string - path to the JSON file (ex: file.json)
-		__objects: dictionary - empty but will store all objects by <class name>.id 
-		(ex: to store a BaseModel object with id=12121212,
-		the key will be BaseModel.12121212)
-	Public instance methods:
-		all(self): returns the dictionary __objects
-		new(self, obj): sets in __objects the obj with key <obj class name>.id
-		save(self): serializes __objects to the JSON file (path: __file_path)
-		reload(self): deserializes the JSON file to __objects
-		(only if the JSON file (__file_path) exists ; otherwise, do nothing.
-		If the file doesn’t exist, no exception should be raised)
-	"""
-	__file_path = "file.json"
-	__objects = {}
+    """
+    Class for storing and retrieving data
+    Private class attributes:
+        __file_path: string - path to the JSON file (ex: file.json)
+        __objects: dictionary - empty but will store all objects by <class name>.id 
+        (ex: to store a BaseModel object with id=12121212,
+        the key will be BaseModel.12121212)
+    Public instance methods:
+        all(self): returns the dictionary __objects
+        new(self, obj): sets in __objects the obj with key <obj class name>.id
+        save(self): serializes __objects to the JSON file (path: __file_path)
+        reload(self): deserializes the JSON file to __objects
+        (only if the JSON file (__file_path) exists ; otherwise, do nothing.
+        If the file doesn’t exist, no exception should be raised)
+    """
+    def __init__(self):
+        self._FileStorage__file_path = "file.json"
+        self._FileStorage__objects = {}
 
-	def all(self):
-		return FileStorage.__objects
+    def all(self):
+        return self._FileStorage__objects
 
-	def new(self, obj):
-		key = "{}.{}".format(type(obj).__name__, obj.id)
-		FileStorage.__objects[key] = obj
+    def new(self, obj):
+        key = "{}.{}".format(type(obj).__name__, obj.id)
+        self._FileStorage__objects[key] = obj
 
-	def save(self):
+    def save(self):
+        with open(self._FileStorage__file_path, "w") as file:
+            json.dump(self._FileStorage__objects, file)
 
-	def reload(self):
+    def reload(self):
+        try:
+            with open(self._FileStorage__file_path, "r") as file:
+                self._FileStorage__objects = json.load(file)
+        except:
+            pass
