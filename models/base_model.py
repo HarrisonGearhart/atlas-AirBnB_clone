2f38e8a82f7fcce127fde056aaa59e3b8a39dd0c
Tsimmuaj <tyang4204@gmail.com>
2024-09-30 16:32:13 -0500

    date time mess

--- models/base_model.py
+++ models/base_model.py
@@ -18,7 +18,7 @@ class BaseModel:
                     )
                 elif key == "updated_at":
                     self.__dict__["updated_at"] = datetime.strptime(
-                        kwargs["created_at"], "%Y-%m-%dT%H:%M:%S.%f"
+                        kwargs["updated_at"], "%Y-%m-%dT%H:%M:%S.%f"
                     )
                 else:
                     self.__dict__[key] = kwargs[key]
