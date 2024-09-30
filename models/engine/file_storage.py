901a8e5b1accea42c60cb1fcaec8fecdd28f3b52
Tsimmuaj <tyang4204@gmail.com>
2024-09-30 15:48:52 -0500

    changed to the same dictionary

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -33,7 +33,7 @@ class FileStorage:
         self._FileStorage__objects[key] = obj
 
     def save(self):
-        dict = {}
+        dict = self._FileStorage__objects
         for key, value in self._FileStorage__objects.items():
             dict[key] = value.to_dict()
         with open(self._FileStorage__file_path, "w") as file:
