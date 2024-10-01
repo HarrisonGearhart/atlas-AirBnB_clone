069cbace609a15b0caafc03c92913f8b6f5ed717
Tsimmuaj <tyang4204@gmail.com>
2024-10-01 10:56:26 -0500

    test file path

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -7,4 +7,6 @@ from models.engine.file_storage import FileStorage
 
 
 class test(unittest.TestCase):
-    
+    def test__file_path(self):
+        obj = FileStorage()
+        self.assertEqual(obj.__file_path, "file.json")
