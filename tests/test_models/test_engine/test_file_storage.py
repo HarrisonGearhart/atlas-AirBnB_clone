ec8da3cb60ef131ac2d701f3b4b6cc6aa66989aa
Tsimmuaj <tyang4204@gmail.com>
2024-10-01 14:35:54 -0500

    make test object

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -6,9 +6,12 @@ import unittest
 import models
 import os
 
+import models.base_model
+
 
 class test(unittest.TestCase):
     def test__file_path(self):
+        test = models.base_model
         my_file = models.storage._FileStorage__file_path
         path = os.getcwd() + "/" + my_file
         self.assertTrue(os.path.isfile(path))
