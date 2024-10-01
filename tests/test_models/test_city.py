8146c81e65a20cbbdc9beb46b305e215217e1636
Harrison  Gearhart <harrisongearhart@Harrisons-MacBook-Air.local>
2024-10-01 15:33:31 -0500

    HGOct1 Updated test_city

--- tests/test_models/test_city.py
+++ tests/test_models/test_city.py
@@ -7,8 +7,10 @@ from models.city import City
 
 
 class test(unittest.TestCase):
-
-
+    def test_state_id_is_str(self):
+        self.assertEqual(str, type(City.state_id))
+    def test_name_is_str(self):
+        self.assertEqual(str, type(City.name))
 
 if __name__ == "__main__":
     unittest.main()
