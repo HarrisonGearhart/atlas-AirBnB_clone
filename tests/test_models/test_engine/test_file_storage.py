f674d715ae1c739150daf1eb72d99058394dc1d0
Tsimmuaj <tyang4204@gmail.com>
2024-10-01 15:31:57 -0500

    test file.json

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -9,12 +9,7 @@ import models
 
 class test(unittest.TestCase):
     def test__file_path(self):
-        a = models.engine.file_storage.FileStorage()
-    try:
-        with open(a._FileStorage__file_path, "r") as file:
-            self.assertTrue(a._FileStorage__file_path)
-    except:
-        pass
+        self.assertTrue("file.json")
 
 if __name__ == "__main__":
     unittest.main()
