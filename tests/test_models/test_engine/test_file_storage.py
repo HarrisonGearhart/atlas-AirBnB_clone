4ce6bcf5b54430f7e56be9d9d7759ee51ac6390b
Tsimmuaj <tyang4204@gmail.com>
2024-10-01 14:13:48 -0500

    rm useless object

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -8,7 +8,6 @@ import models
 
 class test(unittest.TestCase):
     def test__file_path(self):
-        garb = models.base_model
         self.assertEqual(models.storage._FileStorage__file_path, "file.json")
 
 if __name__ == "__main__":
