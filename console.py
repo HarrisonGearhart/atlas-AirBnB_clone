35d54d38760934c62ad2db9fdc0d4ed79cf33ba0
Harrison  Gearhart <harrisongearhart@Harrisons-MacBook-Air.local>
2024-10-01 13:24:14 -0500

    pycode console HGOct1

--- console.py
+++ console.py
@@ -124,7 +124,7 @@ class HBNBCommand(cmd.Cmd):
 
     def do_update(self, *arg):
         """
-        do-update: Updates an instance based on the class name/id 
+        do-update: Updates an instance based on the class name/id
         by adding or updating attribute (save the change into the JSON file)
         Ex: $ update BaseModel 1234-1234-1234 email "aibnb@mail.com".
         The attribute value must be casted to the attribute type
