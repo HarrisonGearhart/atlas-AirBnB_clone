a279b701a28a3cbbbf9c127dd65a2ce350cf16d4
Tsimmuaj <tyang4204@gmail.com>
2024-10-01 11:26:58 -0500

    changed test for file path

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -9,4 +9,11 @@ from models.engine.file_storage import FileStorage
 class test(unittest.TestCase):
     def test__file_path(self):
         obj = FileStorage()
-        self.assertEqual(obj.__file_path, "file.json")
+        try:
+            with open("file.json", "r") as file:
+                self.assertDictEqual(obj, FileStorage)
+        except:
+            pass
+
+if __name__ == "__main__":
+    unittest.main()
