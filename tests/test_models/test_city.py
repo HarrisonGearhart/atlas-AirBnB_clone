3fc99f5048b5057003c5a953db94d4e5bf31e553
Harrison  Gearhart <harrisongearhart@Harrisons-MacBook-Air.local>
2024-10-01 14:07:30 -0500

    HGOct1 create all test models

---
+++ tests/test_models/test_city.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+file to hold unit test class
+"""
+import unittest
+from models.city import City
+
+
+class test(unittest.TestCase):
+
+
+
+if __name__ == "__main__":
+    unittest.main()
