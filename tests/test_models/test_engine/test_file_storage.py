60a2810cb658538f2cd076699047abcf1eef8c3f
Tsimmuaj <tyang4204@gmail.com>
2024-10-01 14:12:07 -0500

    got filestorage file path

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -3,13 +3,13 @@
 file to hold unit test class
 """
 import unittest
-from models.engine.file_storage import FileStorage
+import models
 
 
 class test(unittest.TestCase):
     def test__file_path(self):
-        garb = FileStorage()
-        self.assertEqual(self._FileStorage__file_path, "file.json")
+        garb = models.base_model
+        self.assertEqual(models.storage._FileStorage__file_path, "file.json")
 
 if __name__ == "__main__":
     unittest.main()
